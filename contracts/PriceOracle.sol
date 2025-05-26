02f89857362a06b6168b0237de7c6d2c4835ee09
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2025-05-26 12:53:09 +0600

    price updaing

--- contracts/PriceOracle.sol
+++ contracts/PriceOracle.sol
@@ -11,7 +11,7 @@ contract PriceOracle {
     
 
     mapping (address => uint256) public prices;
-    mapping (address => address) public priceUpdates;
+    mapping (address => address) public priceUpdaters;
     mapping (address => uint256) public lastUpdateTimestamp;
     uint256 public constant PRICE_EXPIRATION = 24 hours;
     
@@ -27,6 +27,27 @@ contract PriceOracle {
 
     constructor() {
         owner = msg.sender;
+    }
+
+    event PriceUpdated(address indexed token, uint256 price);
+    event PriceUpdaterSet(address indexed token, address indexed updater);
+    event PriceUpdated(address indexed token, uint256 price, uint256 timestamp);
+
+    struct Price {
+        uint256 price;
+        uint256 timestamp;
+        bool success;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function setPriceUpdater(address _token, address _updater) external onlyOwner {
+        require(_token != address(0), "Invalid token address");
+        require(_updater != address(0), "Invalid updater address");
+        priceUpdaters[_token] = _updater;
+        emit PriceUpdaterSet(_token, _updater);
     }
 
 }
