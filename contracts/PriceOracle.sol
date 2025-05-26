84158aaa8bf975d8b91078d1ab929c2df22f7a5e
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2025-05-26 12:31:35 +0600

    changing

--- contracts/PriceOracle.sol
+++ contracts/PriceOracle.sol
@@ -9,4 +9,24 @@ contract PriceOracle {
         _;
     }
     
+
+    mapping (address => uint256) public prices;
+    mapping (address => address) public priceUpdates;
+    mapping (address => uint256) public lastUpdateTimestamp;
+    uint256 public constant PRICE_EXPIRATION = 24 hours;
+    
+    event PriceUpdated(address indexed token, uint256 price);
+    event PriceUpdaterSet(address indexed token, address indexed updater);
+    event PriceUpdated(address indexed token, uint256 price, uint256 timestamp);
+
+    struct Price {
+        uint256 price;
+        uint256 timestamp;
+        bool success;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
 }
